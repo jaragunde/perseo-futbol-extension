bb92be4efac5cde4bfb31e40111ef0450bf238b8
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2022-05-11 15:56:35 +0200

    Check site status every minute.
    We would check site status every minute and switch icon between color/
    grey.

--- background.js
+++ background.js
@@ -5,20 +5,33 @@ const colorIcon = {
     "128": "icons/favicon.ico"
   }
 };
+const greyIcon = {
+  path: {
+    "128": "icons/favicon-grey.png"
+  }
+};
 
 chrome.browserAction.onClicked.addListener(function (activeTab) {
   chrome.tabs.create({ url: SITE });
 });
 
-fetch(SITE + "/portada").then(function (response) {
-  if (response.ok) {
-    return response.text();
-  }
-}).then(function (text) {
-  const parser = new DOMParser();
-  const xmlDocument = parser.parseFromString(text, 'text/html');
-  const glyph = xmlDocument.querySelector(".disputar-jornada > .glyphicon");
-  if (glyph && glyph.classList.contains("glyphicon-play")) {
-    chrome.browserAction.setIcon(colorIcon);
-  }
-});
+function checkSiteStatus() {
+  fetch(SITE + "/portada").then(function (response) {
+    if (response.ok) {
+      return response.text();
+    }
+  }).then(function (text) {
+    const parser = new DOMParser();
+    const xmlDocument = parser.parseFromString(text, 'text/html');
+    const glyph = xmlDocument.querySelector(".disputar-jornada > .glyphicon");
+    if (glyph && glyph.classList.contains("glyphicon-play")) {
+      chrome.browserAction.setIcon(colorIcon);
+    }
+    else if (glyph && glyph.classList.contains("glyphicon-ok")) {
+      chrome.browserAction.setIcon(greyIcon);
+    }
+  });
+}
+
+checkSiteStatus();
+window.setInterval(checkSiteStatus, 60000 /* 1 min */);
