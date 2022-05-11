5205a66a9577989e53df586daea57eb06c76d356
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2022-05-11 13:39:14 +0200

    Change icon color depending on game status.
    Grey: logged out or waiting for players.
    Color: ready to play.

--- background.js
+++ background.js
@@ -1,5 +1,24 @@
-chrome.browserAction.onClicked.addListener(function(activeTab)
-{
-    var newURL = "https://pf-env.eba-2fntvvpi.eu-west-3.elasticbeanstalk.com/";
-    chrome.tabs.create({ url: newURL });
+const SITE = "https://pf-env.eba-2fntvvpi.eu-west-3.elasticbeanstalk.com/";
+
+const colorIcon = {
+  path: {
+    "128": "icons/favicon.ico"
+  }
+};
+
+chrome.browserAction.onClicked.addListener(function (activeTab) {
+  chrome.tabs.create({ url: SITE });
+});
+
+fetch(SITE + "/portada").then(function (response) {
+  if (response.ok) {
+    return response.text();
+  }
+}).then(function (text) {
+  const parser = new DOMParser();
+  const xmlDocument = parser.parseFromString(text, 'text/html');
+  const glyph = xmlDocument.querySelector(".disputar-jornada > .glyphicon");
+  if (glyph && glyph.classList.contains("glyphicon-play")) {
+    chrome.browserAction.setIcon(colorIcon);
+  }
 });
